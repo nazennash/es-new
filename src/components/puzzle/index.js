44963d0d587f838b6bf18dade121c01b9d01440c
nazennash <nazennash120gmail.com>
2025-02-03 16:35:11 +0300

    puzzle-page-removed

--- src/components/puzzle/index.js
+++
@@ -1,4 +0,0 @@
-export { default as JigsawPuzzle } from './JigsawPuzzle';
-export { default as SlidingPuzzle } from './SlidingPuzzle';
-export { default as RotatingPuzzle } from './RotatingPuzzle';
-export { default as MemoryPuzzle } from './MemoryPuzzle';
